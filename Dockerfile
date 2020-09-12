b59bb80f8878c45ead30be683b294144a84f2781
Luca Carlon <carlon.luca@gmail.com>
2020-09-12 13:52:33 +0200

    Run entrypoint as root and configure uid and gid as requested.

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,6 @@ RUN \
     rm -rf /var/log/mldonkey && \
     rm /var/lib/mldonkey/*
 
-USER mldonkey
 ENV MLDONKEY_DIR=/var/lib/mldonkey LC_ALL=C.UTF-8 LANG=C.UTF-8
 VOLUME /var/lib/mldonkey
 EXPOSE 4000 4080 19040 19044
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,30 @@
 #!/bin/sh
 
+if [ ! -f /root/initialized ]; then
+	if [ ! -z "$MLDONKEY_UID" ]; then
+    		echo "Resetting mldonkey uid to $MLDONKEY_UID"
+    		usermod -u $MLDONKEY_UID mldonkey
+	fi
+
+	if [ ! -z "$MLDONKEY_GID" ]; then
+    		echo "Resetting mldonkey gid to $MLDONKEY_GID"
+    		groupmod -g $MLDONKEY_GID mldonkey
+	fi
+
+	touch /root/initialized
+fi
+
 if [ ! -f /var/lib/mldonkey/downloads.ini ]; then
-    mldonkey &
+    su -s /bin/bash -c 'mldonkey' - mldonkey &
     echo "Waiting for mldonkey to start..."
     sleep 5
-    /usr/lib/mldonkey/mldonkey_command -p "" "set allowed_ips 0.0.0.0/0" "save"
+    su -s /bin/bash -c '/usr/lib/mldonkey/mldonkey_command -p "" "set allowed_ips 0.0.0.0/0" "save"' mldonkey &
     if [ -z "$MLDONKEY_ADMIN_PASSWORD" ]; then
-        /usr/lib/mldonkey/mldonkey_command -p "" "kill"
+        su -s /bin/bash -c '/usr/lib/mldonkey/mldonkey_command -p "" "kill"' - mldonkey
     else
-        /usr/lib/mldonkey/mldonkey_command -p "" "useradd admin $MLDONKEY_ADMIN_PASSWORD"
-        /usr/lib/mldonkey/mldonkey_command -u admin -p "$MLDONKEY_ADMIN_PASSWORD" "kill"
+        su -s /bin/bash -c '/usr/lib/mldonkey/mldonkey_command -p "" "useradd admin $MLDONKEY_ADMIN_PASSWORD"' - mldonkey
+        su -s /bin/bash -c '/usr/lib/mldonkey/mldonkey_command -u admin -p "$MLDONKEY_ADMIN_PASSWORD" "kill"' - mldonkey
     fi
 fi
 
-mldonkey 
+su -s /bin/bash -c 'mldonkey' - mldonkey
